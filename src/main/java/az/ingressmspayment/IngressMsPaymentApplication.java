2e645ab6dc41c750db222aa0bc8931000cc5b6af
AliUSA01 <alimammadov873@gmail.com>
2025-04-12 00:11:09 +0400

    Payment ms

---
+++ src/main/java/az/ingressmspayment/IngressMsPaymentApplication.java
@@ -0,0 +1,13 @@
+package az.ingressmspayment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IngressMsPaymentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IngressMsPaymentApplication.class, args);
+    }
+
+}
